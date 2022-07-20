c9debe6c5e394f332ecd99483116a4fc304bc055
yacine <yacinenaitamer@yahoo.fr>
2022-07-20 15:14:50 +0200

    espace

--- Angular/my-first-app/src/app/user/user-register/user-register.component.ts
+++ Angular/my-first-app/src/app/user/user-register/user-register.component.ts
@@ -15,6 +15,7 @@ export class UserRegisterComponent implements OnInit {
   ngOnInit() {
 
     this.registerationForm = new FormGroup({
+
     userName: new FormControl('Mark', Validators.required),
     email: new FormControl(null, [Validators.required, Validators.email]),
     password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
